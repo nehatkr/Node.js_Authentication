647afb123964a32bbcd66279a87f0d7b0d8d25f0
Nehatkr <nehathakur2308@gmail.com>
2025-09-17 12:40:54 +0530

    first commit

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const connectToDB = require('./database/db')
+const authRoutes = require('./routes/auth-routes')
 connectToDB();
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json())
+
+app.use('/api/auth', authRoutes)
 app.listen(PORT, ()=>{
     console.log(`Server is now running on port : ${PORT}`)
 })
