1f4222f06ee01095b7d3f599a522198c3ccece7e
Nehatkr <nehathakur2308@gmail.com>
2025-09-21 16:21:42 +0530

    another middleware added

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const authmiddleware = require('../middleware/auth-middleware')
+const adminMiddleware = require('../middleware/admin-middleware')
 
 const router = express.Router();
 
-router.get('/welcome', authmiddleware, (req,res)=>{
+router.get('/welcome', authmiddleware, adminMiddleware, (req,res)=>{
     res.json({
         message: 'Welcome to the admine page'
     })
